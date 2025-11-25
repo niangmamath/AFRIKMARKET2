6a4c83af41a05f33607daa1c610713746c692c86
mouhamadn63 <mouhamadn63@gmail.com>
2025-11-25 17:07:11 +0000

    résolution de problème avec cloudinary

--- config/cloudinary.ts
+++ config/cloudinary.ts
@@ -3,26 +3,23 @@ import { v2 as cloudinary } from 'cloudinary';
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
 import multer from 'multer';
 import './env'; // Load environment variables
-import { Request } from 'express';
 
-// Configure Cloudinary with credentials from environment variables
+// Configure Cloudinary
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-    secure: true // Use https
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME as string,
+    api_key: process.env.CLOUDINARY_API_KEY as string,
+    api_secret: process.env.CLOUDINARY_API_SECRET as string,
+    secure: true
 });
 
 // Configure Multer to use Cloudinary for storage
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
-    params: async (req: Request, file: Express.Multer.File) => {
-        // You can add folder and format logic here
-        return {
-            folder: 'leboncoin', // Folder name on Cloudinary
-            allowed_formats: ['jpeg', 'png', 'jpg', 'gif']
-        };
-    }
+    params: {
+        folder: 'leboncoin',
+        // The type for allowedFormats is string[]
+        allowedFormats: ['jpeg', 'png', 'jpg', 'gif'],
+    } as { [key: string]: any }, // Type assertion to match expected params
 });
 
 // Create the multer upload instance
